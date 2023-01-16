609031152d4753fb9ffeb16d6a215ed692619845
Hithru <hithrudealwis@gmail.com>
2023-01-16 13:05:08 +0530

    Initialize folder structure for deployed function

---
+++ management-system-api/api/src/adapters/license.ts
@@ -0,0 +1,11 @@
+export namespace LicenseAdapter {
+  export async function create() {
+    const response = "Create the license";
+    return response;
+  }
+
+  export async function deploy() {
+    const response = "Deployed the license";
+    return response;
+  }
+}
--- management-system-api/api/src/model/Common.ts
+++
@@ -1,4 +0,0 @@
-export enum LicenseTypes {
-  PERPETUAL = "PERPETUAL",
-  SUBSCRIPTION = "SUBSCRIPTION",
-}
