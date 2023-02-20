c0e6fd58742ee0e149ee85f50e711d3ab8b74592
Hithru <hithrudealwis@gmail.com>
2023-02-20 15:48:49 +0530

    Update dynamoDB querying and deleting

--- management-system-api/api/src/core/entities/database/dbUtil.ts
+++ management-system-api/api/src/core/entities/database/dbUtil.ts
@@ -1,9 +1,24 @@
 import { internal, ErrorCode } from "../../model";
 
 export interface DBUtil {
+  // query functions
+  getOneByPrimaryKey(tableName: string, keyValueObject: any): Promise<any>;
+  queryTable(
+    tableName: string,
+    query: internal.DBQuery,
+    primaryKeyFieldName: any,
+    indexKeyFieldName: string,
+    primaryKey?: any,
+    indexKey?: string
+  ): Promise<any>;
+  scanTable(tableName: string, query: internal.DBQuery): Promise<any>;
+
   // insert/update functions
   writeOneToDB(tableName: string, item: any): Promise<any>;
   transactWriteDB(params: any): Promise<any>;
+
+  // delete functions
+  deleteItem(tableName: string, keyValueObject: any): Promise<any>;
 }
 
 export class DBError extends Error {
--- management-system-api/api/src/core/entities/database/dynamoDbUtil.ts
+++ management-system-api/api/src/core/entities/database/dynamoDbUtil.ts
@@ -1,12 +1,23 @@
 import AWS from "aws-sdk";
 import {
+  DeleteItemInput,
   PutItemInput,
+  GetItemOutput,
   PutItemInputAttributeMap,
+  PutItemOutput,
+  QueryInput,
+  ScanInput,
+  ScanOutput,
   TransactWriteItemsInput,
   TransactWriteItemsOutput,
 } from "aws-sdk/clients/dynamodb";
 import { DBUtil, DBError } from "./dbUtil";
+import { internal } from "../../model";
 
+interface CompositeKey {
+  partitionKey: string;
+  rangeKey?: string;
+}
 export class DynamoDbUtil implements DBUtil {
   private dynamoDb: AWS.DynamoDB.DocumentClient;
   private static dynamoDbUtil: DynamoDbUtil;
@@ -22,6 +33,103 @@ export class DynamoDbUtil implements DBUtil {
     }
 
     return DynamoDbUtil.dynamoDbUtil;
+  }
+
+  // query utils
+  async getOneByPrimaryKey(tableName: string, keyValueObject: any) {
+    let params: AWS.DynamoDB.DocumentClient.GetItemInput = {
+      Key: keyValueObject,
+      TableName: tableName,
+    };
+    try {
+      let res = await this.dynamoDb.get(params).promise();
+      console.log("Queried item: ", res.Item);
+      return res.Item;
+    } catch (e) {
+      console.error(
+        `TableName: ${tableName}, Operation: getOneByPrimaryKey, Error: `,
+        e
+      );
+      throw new DBError(
+        `Error while getOneByPrimaryKey in ${tableName} : ${e} `
+      );
+    }
+  }
+
+  async queryTable(
+    tableName: string,
+    query: internal.DBQuery,
+    primaryKeyFieldName: CompositeKey,
+    indexKeyFieldName: string,
+    primaryKey?: CompositeKey,
+    indexKey?: string
+  ): Promise<ScanOutput> {
+    let params: QueryInput;
+    let res: any;
+    if (primaryKey && primaryKey.partitionKey) {
+      params = this.getQueryParams(
+        tableName,
+        primaryKeyFieldName,
+        primaryKey,
+        query.limit,
+        query.offset
+      );
+    } else {
+      params = this.getIndexQueryParam(
+        tableName,
+        indexKeyFieldName,
+        indexKey,
+        query.limit,
+        query.offset
+      );
+    }
+    try {
+      res = await this.dynamoDb.query(params).promise();
+      console.log("Queried items: ", res);
+    } catch (e) {
+      console.error(
+        `TableName: ${tableName}, Operation: queryTable, Error: `,
+        e
+      );
+      throw new DBError(` Error while queryTable in ${tableName} : ${e} `);
+    }
+    return res;
+  }
+
+  async scanTable(
+    tableName: string,
+    query: internal.DBQuery
+  ): Promise<ScanOutput> {
+    let param = this.getScanParam(tableName!, query!.limit, query!.offset);
+    let res: AWS.DynamoDB.DocumentClient.ScanOutput;
+    let cache: any = [];
+    let remining: number;
+    let totalRecords = param.Limit!;
+    try {
+      do {
+        res = await this.dynamoDb.scan(param).promise();
+        cache = [...cache, ...res.Items!];
+        remining = totalRecords - cache.length!;
+        console.log(`scaned ${cache.length} items and remininig ${remining}`);
+        if (res?.LastEvaluatedKey !== undefined) {
+          param.ExclusiveStartKey = res!.LastEvaluatedKey;
+          param.Limit = remining;
+        }
+      } while (
+        cache.length! < totalRecords &&
+        res.LastEvaluatedKey !== undefined
+      );
+      res.Items! = cache;
+    } catch (e) {
+      console.error(
+        `TableName: ${tableName}, Operation: scanTable, Error: `,
+        e
+      );
+      throw new DBError(` Error while scanTable in ${tableName} : ${e} `);
+    }
+
+    console.log("Queried items: ", res);
+    return res;
   }
 
   // write utils
@@ -54,10 +162,115 @@ export class DynamoDbUtil implements DBUtil {
     }
   }
 
+  // delete functions
+  async deleteItem(tableName: string, keyValueObject: any): Promise<any> {
+    try {
+      let param = this.getDeleteParam(tableName!, keyValueObject);
+      let res = await this.dynamoDb.delete(param).promise();
+      console.log("DB Response: ", res);
+      return res.Attributes;
+    } catch (e) {
+      console.error(
+        `TableName: ${tableName}, Operation: deleteItem, Error: `,
+        e
+      );
+      throw new DBError(` Error while deleteItem in ${tableName} : ${e} `);
+    }
+  }
+
+  // other utils
+  getQueryParams(
+    tableName: string,
+    key: CompositeKey,
+    value: CompositeKey,
+    limit: any = 100,
+    offset: any = null
+  ) {
+    let params: QueryInput;
+    if (typeof limit != "number") {
+      throw ` limit value must be number not ${typeof limit} `;
+    }
+
+    let keyConditionExpression = [];
+    const expressionAttributeNames: any = {};
+    const expressionAttributeValues: any = {};
+
+    if (value.partitionKey != null) {
+      keyConditionExpression.push("#partitionKey=:partitionValue");
+      expressionAttributeNames["#partitionKey"] = key.partitionKey;
+      expressionAttributeValues[":partitionValue"] = value.partitionKey;
+    }
+
+    if (value.rangeKey != null) {
+      keyConditionExpression.push("#rangeKey=:rangeValue");
+      expressionAttributeNames["#rangeKey"] = key.rangeKey;
+      expressionAttributeValues[":rangeValue"] = value.rangeKey;
+    }
+
+    params = {
+      TableName: tableName,
+      KeyConditionExpression: keyConditionExpression.join(" AND "),
+      ExpressionAttributeNames: expressionAttributeNames,
+      ExpressionAttributeValues: expressionAttributeValues,
+      Limit: limit,
+      ExclusiveStartKey: offset,
+    };
+    return params;
+  }
+
+  getIndexQueryParam(
+    tableName: string,
+    indexKey: string,
+    value: any,
+    limit: any = 100,
+    offset: any = null
+  ) {
+    let params: QueryInput;
+    if (typeof limit != "number") {
+      throw ` limit value must be number not ${typeof limit} `;
+    }
+    params = {
+      TableName: tableName,
+      IndexName: `${indexKey}-index`,
+      KeyConditionExpression: "#index =:value",
+      ExpressionAttributeNames: {
+        "#index": indexKey,
+      },
+      ExpressionAttributeValues: {
+        ":value": value,
+      },
+      Limit: limit,
+      ExclusiveStartKey: offset,
+    };
+    return params;
+  }
+
+  getScanParam(tableName: string, limit: any = 100, offset: any = null) {
+    if (typeof limit != "number") {
+      throw ` limit value must be number not ${typeof limit} `;
+    }
+    let params: ScanInput;
+    params = {
+      TableName: tableName,
+      Limit: limit,
+      ExclusiveStartKey: offset,
+    };
+    return params;
+  }
+
   getPutItemParams(tableName: string, item: PutItemInputAttributeMap) {
     let params: PutItemInput = {
       TableName: tableName,
       Item: item,
+      ReturnValues: "ALL_OLD",
+    };
+    return params;
+  }
+
+  getDeleteParam(tableName: string, key: any) {
+    let params: DeleteItemInput = {
+      TableName: tableName,
+      Key: key,
       ReturnValues: "ALL_OLD",
     };
     return params;
